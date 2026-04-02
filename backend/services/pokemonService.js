d30370fe01fb052a221c67e8685972dc9e0be469
zoharyem <126849959+zoharyem@users.noreply.github.com>
2026-04-02 15:34:11 +1100

    Made a basic function to get a short list of pokemon from the Pokeapi

--- backend/services/pokemonService.js
+++ backend/services/pokemonService.js
@@ -3,6 +3,7 @@
     How do we get the data?
 */
 
+// A function that fetches a pokemon by name from the PokeAPI.
 export const fetchPokemon = async (name) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     const data = await response.json()
@@ -14,6 +15,20 @@ export const fetchPokemon = async (name) => {
         stats: data.stats.map(s => ({
             name: s.stat.name,
             value: s.base_stat
+        }))
+    }
+}
+
+// A function that fetches a list of the user's pokemon options from the PokeAPI.
+// Only name and image are stored for efficency.
+export const fetchPokemonOptions = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=250')
+    const data = await response.json()
+
+    return {
+        data: data.results.map(p => ({
+            name: p.name,
+            image: data.sprites.front_default
         }))
     }
 }
