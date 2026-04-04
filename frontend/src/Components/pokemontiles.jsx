8c13f50182c767d9a7318754b9352a9c1bdf5279
zoharyem <126849959+zoharyem@users.noreply.github.com>
2026-04-04 17:51:25 +1100

    Added coll animation for pokeball

--- frontend/src/Components/pokemontiles.jsx
+++ frontend/src/Components/pokemontiles.jsx
@@ -1,10 +1,27 @@
 import { Tooltip } from 'react-tooltip';
+import { useRef, useState } from 'react';
 
 export function PokemonTile({ pokemon, onClick }) {
+    const [clickCount, setClickCount] = useState(0);
+    const [stateChanged, setStateChanged] = useState(false);
+
+    const MAX_CLICKS = 3;
+
+    const handleClick = () => {
+        if (clickCount < MAX_CLICKS) {
+        setClickCount(clickCount + 1);
+        } else {
+            setStateChanged(true);
+            setClickCount(0);
+        }
+    }
+
     if (!pokemon) {
         return (
-            <div className='pokemon-tile'>
-                <img src="/poke-ball.png" alt="Empty slot" />
+            <div className='pokemon-tile' onClick = {handleClick}>
+                <div className={`pokemon-tile-image ${clickCount > 0 ? `shake${clickCount}` : ''}`}>
+                    <img src="/poke-ball.png" alt="Empty slot" />
+                </div>
             </div>
         );
     }
